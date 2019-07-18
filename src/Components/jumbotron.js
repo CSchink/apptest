d8413a637643c5cca74c99d66d217bdafdaad3b8
CSchink <45950072+CSchink@users.noreply.github.com>
2019-07-18 12:30:38 -0400

    Jumbotron Edit

--- src/Components/jumbotron.js
+++ src/Components/jumbotron.js
@@ -4,20 +4,20 @@ import { MDBJumbotron, MDBBtn, MDBContainer, MDBRow, MDBCol, MDBCardTitle, MDBIc
 const JumbotronPage = () => {
   return (
     <div>
-      {/* // <MDBContainer> */}
+      // <MDBContainer>
       <MDBRow>
         <MDBCol>
           <MDBJumbotron style={{ padding: 0 }}>
             <MDBCol className="text-black text-center py-5 px-4 my-5" style={{ backgroundImage: `url(http://www.testnec.ro/wp-content/uploads/2019/01/imagine-2.jpg)` }}>
               <MDBCol className="py-5">
-                <MDBCardTitle className="h1-responsive pt-3 m-5 font-bold">Welcome to SOTT Lab</MDBCardTitle>
+                <MDBCardTitle className="h1-responsive pt-3 m-5 font-bold"></MDBCardTitle>
                 {/* <MDBBtn outline color="black" className="mb-5"><MDBIcon icon="clone" className="mr-2"></MDBIcon> View project</MDBBtn> */}
               </MDBCol>
             </MDBCol>
           </MDBJumbotron>
         </MDBCol>
       </MDBRow>
-      {/* // </MDBContainer> */}
+      // </MDBContainer>
     </div>
   )
 }
