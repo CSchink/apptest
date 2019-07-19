fd5d08faa3771498f77f74863f26bdbb99d13808
CSchink <45950072+CSchink@users.noreply.github.com>
2019-07-19 14:05:28 -0400

    new login form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
-import { Menu, Icon } from 'antd';
 // import "@fortawesome/fontawesome-free/css/all.min.css";
 // import "bootstrap-css-only/css/bootstrap.min.css";
 // import "mdbreact/dist/css/mdb.css";
@@ -11,7 +10,7 @@ import SottDashBoard from './Components/sottdashboards';
 import JumbotronPage from './Components/jumbotron';
 import SottTable from './Components/sottTable';
 import InputPage from './Components/inputPage';
-import Login from './Components/Login';
+import LoginForm from './Components/Login';
 import NavBar from './Components/navBar';
 
 class App extends Component {
@@ -34,7 +33,7 @@ class App extends Component {
             <div className="collpase nav-collapse">
               <ul className="navbar-nav mr-auto">
                 <li className="navbar-item">
-                  <Link to="/" className="nav-link">Home</Link>
+                  <Link to="/sottlab" className="nav-link">Home</Link>
                 </li>
                 <li className="navbar-item">
                   <Link to="/dashboards" className="nav-link">Dashboards</Link>
@@ -48,7 +47,7 @@ class App extends Component {
       <JumbotronPage />
       <Route path ="/dashboards" component ={SottDashBoard} />
       <Route path="/input" component={InputPage} />
-      <Route path="/" exact component ={Login} />
+      <Route path="/" exact component ={LoginForm} />
       <Route path="/sottlab" exact component ={SottTable} />
       
     </div>
