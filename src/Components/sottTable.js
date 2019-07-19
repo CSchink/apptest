755d140cec9dd866144e62124100600721c2f753
CSchink <45950072+CSchink@users.noreply.github.com>
2019-07-19 09:41:27 -0400

    filter fix

--- src/Components/sottTable.js
+++ src/Components/sottTable.js
@@ -44,10 +44,10 @@ class SottTable extends Component {
     render() {
         
         const options = {
-            filterType: 'dropdown',
+            filterType: 'multiselect',
             selectableRows: 'none',
             responsive: 'scroll',
-            rowsPerPage: 10,
+            rowsPerPage: 100,
             download: false,
             rowsPerPageOptions: [10,25,50,100],
             customSort: (data, colIndex, order) => { return data.sort((a, b) => { if (colIndex === 1) { return (new Date(a.data[colIndex]) < new Date(b.data[colIndex]) ? -1: 1 ) * (order === 'desc' ? 1 : -1); } else { return (a.data[colIndex] < b.data[colIndex] ? -1: 1 ) * (order === 'desc' ? 1 : -1); } }); }
