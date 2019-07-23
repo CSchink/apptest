8a880a436158124894ac2724b22200bc128f2e0d
CSchink <45950072+CSchink@users.noreply.github.com>
2019-07-23 14:57:58 -0400

    new props design

--- src/Components/jumbotron.js
+++ src/Components/jumbotron.js
@@ -2,6 +2,7 @@ import React from "react";
 import { MDBJumbotron, MDBBtn, MDBContainer, MDBRow, MDBCol, MDBCardTitle, MDBIcon } from "mdbreact";
 
 const JumbotronPage = () => {
+  const text = props => <h1>{props.text}</h1>
   return (
     <div>
     <MDBContainer>
