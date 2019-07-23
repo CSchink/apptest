882ac525f41d2f5348a2aa1047e45019f2ceced3
CSchink <45950072+CSchink@users.noreply.github.com>
2019-07-23 15:05:56 -0400

    new input page and navbar

--- src/Components/jumbotron.js
+++ src/Components/jumbotron.js
@@ -2,7 +2,6 @@ import React from "react";
 import { MDBJumbotron, MDBBtn, MDBContainer, MDBRow, MDBCol, MDBCardTitle, MDBIcon } from "mdbreact";
 
 const JumbotronPage = () => {
-  const text = props => <h1>{props.text}</h1>
   return (
     <div>
     <MDBContainer>
@@ -11,7 +10,7 @@ const JumbotronPage = () => {
           <MDBJumbotron style={{ padding: 0 }}>
             <MDBCol className="text-black text-center py-5 px-4 my-5" style={{ backgroundImage: `url(http://www.testnec.ro/wp-content/uploads/2019/01/imagine-2.jpg)` }}>
               <MDBCol className="py-5">
-                <MDBCardTitle className="h1-responsive pt-3 m-5 font-bold">{text}</MDBCardTitle>
+                <MDBCardTitle className="h1-responsive pt-3 m-5 font-bold">{props.text}</MDBCardTitle>
                 {/* <MDBBtn outline color="black" className="mb-5"><MDBIcon icon="clone" className="mr-2"></MDBIcon>View Tutorial</MDBBtn> */}
               </MDBCol>
             </MDBCol>
