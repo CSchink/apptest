708e5c531e519260f5868be4c4f02cbe4644d8c0
CSchink <45950072+CSchink@users.noreply.github.com>
2019-07-29 17:24:57 -0400

    login form test

--- functions/authenticate.js
+++ functions/authenticate.js
@@ -32,52 +32,6 @@ exports.handler = function(event, context, callback) {
         },
         {collection:'logindata'} ));
       }
-    
-    logindata.methods.isCorrectPassword = function(password, callback) {
-        bcrypt.compare(password, this.password, function(err, same) {
-            if (err) {
-                callback(err);
-            } else {
-                callback(err, same);
-            }
-        })
-    }
-      const { user , password } = req.body;
-    logindata.findOne({ user }, function(err, user) {
-      if (err) {
-        console.error(err);
-        res.status(500)
-          .json({
-          error: 'Internal error please try again'
-        });
-      } else if (!user) {
-        res.status(401)
-          .json({
-          error: 'Incorrect email or password'
-        });
-      } else {
-        logindata.isCorrectPassword(password, function(err, same) {
-          if (err) {
-            res.status(500)
-              .json({
-              error: 'Internal error please try again'
-            });
-          } else if (!same) {
-            res.status(401)
-              .json({
-              error: 'Incorrect email or password'
-            });
-          } else {
-            // Issue token
-            const payload = { user };
-            const token = jwt.sign(payload, secret, {
-              expiresIn: '1h'
-            });
-            res.cookie('token', token, { httpOnly: true }).sendStatus(200);
-          }
-        });
-      }
-    });
     const M = conn.model('logindata');
 
     const doc = yield M.find();
