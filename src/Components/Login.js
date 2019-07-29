aa6f53169138885009ef772da7b648e55a796b4d
CSchink <45950072+CSchink@users.noreply.github.com>
2019-07-29 17:26:17 -0400

    new login test

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Form, Icon, Input, Button, Checkbox } from 'antd';
 import JumbotronPage from './jumbotron';
 import axios from 'axios';
-import fetch from 'whatwg-fetch';
 
 
 class Login extends React.Component {
@@ -23,7 +22,7 @@ class Login extends React.Component {
 
     onSubmit = (event) => {
     event.preventDefault();
-    fetch('/.netlify/functions/authenticate', {
+    axios.get('/.netlify/functions/authenticate', {
         method: 'POST',
         body: JSON.stringify(this.state),
         headers: {
