7810a956864f979d840650d058a7339811970d62
CSchink <45950072+CSchink@users.noreply.github.com>
2019-08-25 17:41:09 -0400

    new css styling

--- src/Components/tutorialPage.js
+++ src/Components/tutorialPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import YoutubeId from './youtube'
 import JumbotronPage from './jumbotron';
 import { Layout, Menu } from 'antd';
+import '../App.css';
 
 const { Header, Footer, Content } = Layout;
 
@@ -13,17 +14,7 @@ return(
   <div>
     <JumbotronPage text="History Database Tutorial"/>
     <Layout className="layout">
-      <Header>
-      <Menu
-        theme="light"
-        mode="horizontal"
-        defaultSelectedKeys={['2']}
-        style={{ lineHeight: '64px' }}
-      >
-        <Menu.Item key="1">About the App</Menu.Item>
-        <Menu.Item key="2">Video Tutorial</Menu.Item>
-      </Menu>
-      </Header>
+      <Header>Welcome to the home of the future Tutorial Page</Header>
       <Content>Welcome to the home of the future Tutorial Page</Content>
       <Content>Here we'd like to introduce you to the general vision for the Global History Database</Content>
       <Content style={{textAlign: 'center'}}><YoutubeId source='https://www.youtube.com/embed/MckxlP60cnY'/></Content>
