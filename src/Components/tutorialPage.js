99ac0bff2d8f56c708da0b7e53f23f9392bce1b6
CSchink <45950072+CSchink@users.noreply.github.com>
2019-08-25 18:17:33 -0400

    tutorial page css

--- src/Components/tutorialPage.js
+++ src/Components/tutorialPage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import YoutubeId from './youtube'
 import JumbotronPage from './jumbotron';
 import { Layout } from 'antd';
-
+import '../App.css';
 
 const { Footer, Content } = Layout;
 
@@ -17,7 +17,6 @@ return(
       <Content>Welcome to the home of the future Tutorial Page</Content>
       <Content>Here we'd like to introduce you to the general vision for the Global History Database</Content>
       <Content style={{alignItems: 'center'}}><YoutubeId source='https://www.youtube.com/embed/MckxlP60cnY'/></Content>
-      <Footer>For more information watch the video</Footer>
     </Layout>
   </div>
 )
