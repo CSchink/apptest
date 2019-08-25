04baea146bf91660a7f5a231e27e6350ddade028
CSchink <45950072+CSchink@users.noreply.github.com>
2019-08-25 18:11:10 -0400

    tutorial page layout

--- src/Components/tutorialPage.js
+++ src/Components/tutorialPage.js
@@ -4,7 +4,7 @@ import JumbotronPage from './jumbotron';
 import { Layout } from 'antd';
 
 
-const { Header, Footer, Content } = Layout;
+const { Footer, Content } = Layout;
 
 class TutorialPage extends React.Component {
 
@@ -14,10 +14,9 @@ return(
   <div>
     <JumbotronPage text="History Database Tutorial"/>
     <Layout>
-      <Header>Welcome to the home of the future Tutorial Page</Header>
       <Content>Welcome to the home of the future Tutorial Page</Content>
       <Content>Here we'd like to introduce you to the general vision for the Global History Database</Content>
-      <Content style={{textAlign: 'center'}}><YoutubeId source='https://www.youtube.com/embed/MckxlP60cnY'/></Content>
+      <Content style={{alignItems: 'center'}}><YoutubeId source='https://www.youtube.com/embed/MckxlP60cnY'/></Content>
       <Footer>For more information watch the video</Footer>
     </Layout>
   </div>
