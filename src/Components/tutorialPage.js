940ca2017590cae4b30ad93b36ec16c701626ca8
CSchink <45950072+CSchink@users.noreply.github.com>
2019-08-26 11:57:15 -0400

    new tutorial page

--- src/Components/tutorialPage.js
+++ src/Components/tutorialPage.js
@@ -1,5 +1,3 @@
-export default TutorialPage;
-
 import React, { Component } from 'react';
 import YoutubeId from './youtube'
 import JumbotronPage from './jumbotron';
@@ -18,7 +16,7 @@ render() {
 return(
   <div>
     <JumbotronPage text="History Database Tutorial"/>
-      <h1>Welcome to the Global History Database</h1>
+      <h1 style={{fontWeight: 'bold'}}>Welcome to the Global History Database</h1>
        <div style={{ background: '#ffffff'}}></div> 
         <Layout>
           <Header style={{ background: '#ffffff'}}>Here we'll introduce you to the vision for the Global History Database</Header>
@@ -26,7 +24,7 @@ return(
             <Sider style={{ background: '#ffffff'}}></Sider>
             <Content style={{ background: '#ffffff'}}><h2>Statistics</h2>
             <p>The Statistics section of History Lab will be geared toward social science investigations utilizing all quantitative data available through the Database</p>
-            <h2>Database</h2>
+            <h2 style={{fontWeight: 'bold'}}>Database</h2>
             <br />
             <p>The Database will present a chronological view of world history, filtered through tags and categories for ease of understanding.  Think of it as a time machine that can instantly transport you across time and space with the click of a button!</p>
             <p>The Database will be filterable based on specific events, like World War I and other major historical events, as well as by major cultural and global attributes that are shared across time and space, such as:</p>
