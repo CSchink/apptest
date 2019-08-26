01f55d078e651b6259a33e87978a8d4f3d071057
CSchink <45950072+CSchink@users.noreply.github.com>
2019-08-26 11:40:46 -0400

    tutorial page css

--- src/Components/tutorialPage.js
+++ src/Components/tutorialPage.js
@@ -2,8 +2,12 @@ import React, { Component } from 'react';
 import YoutubeId from './youtube'
 import JumbotronPage from './jumbotron';
 import '../App.css';
+import 'antd/dist/antd.css';
+import { Layout } from 'antd';
+
 import { relative } from 'path';
 
+const { Header, Sider, Content } = Layout;
 
 
 class TutorialPage extends React.Component {
@@ -13,26 +17,28 @@ render() {
 return(
   <div>
     <JumbotronPage text="History Database Tutorial"/>
-      <div>
-        <h1>Welcome to the Global History Database</h1>
-        <br />
-        <h3>Here we'll introduce you to the vision for the Global History Database</h3>
-        <h2>Statistics</h2>
-        <p>The Statistics section of History Lab will be geared toward social science investigations utilizing all quantitative data available through the Database</p>
-        <h2>Database</h2>
-        <br />
-        <p>The Database will present a chronological view of world history, filtered through tags and categories for ease of understanding.  Think of it as a time machine that can instantly transport you across time and space with the click of a button!</p>
-        <p>The Database will be filterable based on specific events, like World War I and other major historical events, as well as by major cultural and global attributes that are shared across time and space, such as:</p>
-        <li>Religious, political, military, economic, social, technological, artistic, philosophical, and medical dimensions of human society</li>
-        <li>Natural events like diseases, floods, volcanic eruptions, ice ages, and</li>
-        <li>Individual branches of modern science like psychology, physics, and more</li>
-        <br />
-        <h2>Video Tutorial</h2>
-        <br />
-        <p>That said, below will be (someday soon) a tutorial video explaining how it all works together</p>
-        <br />
-        <p><YoutubeId source='https://www.youtube.com/embed/MckxlP60cnY'/></p>
-      </div>
+      <h1>Welcome to the Global History Database</h1>
+        <Layout>
+          <Header>Here we'll introduce you to the vision for the Global History Database</Header>
+          <Layout>
+            <Sider><h2>Video Tutorial</h2>
+            <br />
+            <p>That said, below will be (someday soon) a tutorial video explaining how it all works together</p>
+            <br />
+            <p><YoutubeId source='https://www.youtube.com/embed/MckxlP60cnY'/></p></Sider>
+            <Content><h2>Statistics</h2>
+            <p>The Statistics section of History Lab will be geared toward social science investigations utilizing all quantitative data available through the Database</p>
+            <h2>Database</h2>
+            <br />
+            <p>The Database will present a chronological view of world history, filtered through tags and categories for ease of understanding.  Think of it as a time machine that can instantly transport you across time and space with the click of a button!</p>
+            <p>The Database will be filterable based on specific events, like World War I and other major historical events, as well as by major cultural and global attributes that are shared across time and space, such as:</p>
+            <li>Religious, political, military, economic, social, technological, artistic, philosophical, and medical dimensions of human society</li>
+            <li>Natural events like diseases, floods, volcanic eruptions, ice ages, and</li>
+            <li>Individual branches of modern science like psychology, physics, and more</li>
+            <br />
+            </Content>
+          </Layout>
+        </Layout>
   </div>
 
 )
