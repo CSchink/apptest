c11d67df893be902bce12d81f3639a3fef88e754
CSchink <45950072+CSchink@users.noreply.github.com>
2019-11-07 16:58:45 -0500

    new landing page

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -31,13 +31,13 @@ class SottBigBalls extends React.Component {
               <MDBCollapse isOpen={this.state.collapse} navbar>
                 <MDBNavbarNav left>
                   <MDBNavItem active>
-                    <MDBNavLink to="#">Home</MDBNavLink>
+                    <MDBNavLink to="/lab">Database</MDBNavLink>
                   </MDBNavItem>
                   <MDBNavItem>
-                    <MDBNavLink to="#">Link</MDBNavLink>
+                    <MDBNavLink to="/dashboards">Tutorial</MDBNavLink>
                   </MDBNavItem>
                   <MDBNavItem>
-                    <MDBNavLink to="#">Profile</MDBNavLink>
+                    <MDBNavLink to="/login">Log In</MDBNavLink>
                   </MDBNavItem>
                 </MDBNavbarNav>
               </MDBCollapse>
@@ -46,15 +46,7 @@ class SottBigBalls extends React.Component {
 
           <MDBView src="http://www.testnec.ro/wp-content/uploads/2019/01/imagine-2.jpg">
             <MDBMask overlay="black-light" className="flex-center flex-column text-white text-center">
-            <h2>Database</h2>
-            <br />
-            <p>The Database will present a chronological view of world history, filtered through tags and categories for ease of understanding.  Think of it as a time machine that can instantly transport you across time and space with the click of a button!</p>
-            <p>The Database will be filterable based on specific events, like World War I and other major historical events, as well as by major cultural and global attributes that are shared across time and space, such as:</p>
-            <li>Religious, political, military, economic, social, technological, artistic, philosophical, and medical dimensions of human society</li>
-            <li>Natural events like diseases, floods, volcanic eruptions, ice ages, and</li>
-            <li>Individual branches of modern science like psychology, physics, and more</li>
-            <br />
-            <h2>Video Tutorial Coming Soon!</h2>
+            <h1>Welcome to SOTT Lab, home of the greatest warriors known to the Cosmos</h1>
             </MDBMask>
           </MDBView>
         </header>
@@ -64,6 +56,11 @@ class SottBigBalls extends React.Component {
             <p align="justify">Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           </MDBContainer>
         </main>
+        <Route path ="/dashboards" component ={Dashboard} />
+      <Route path="/input" component={InputPage} />
+      <Route path="/" exact component ={SottBigBalls} />
+      <Route path="/lab" exact component ={Table} />
+      <Route path="/tutorial" component={TutorialPage} />
       </div>
     );
   }
