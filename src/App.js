82312127ed43705fcb2476d10a9e23d35d81c6e1
CSchink <45950072+CSchink@users.noreply.github.com>
2019-11-07 17:36:54 -0500

    new landing page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import 'antd/dist/antd.css';
+import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavbarToggler, MDBCollapse, MDBNavItem, MDBNavLink, MDBContainer, MDBMask, MDBView } from 'mdbreact';
 
 import Dashboard from './Components/dashboards';
 import Table from './Components/Table';
@@ -15,7 +16,7 @@ class App extends Component {
     <Router>
     
     <div>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        {/* <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
             <Link to="/lab" className="navbar-brand">SOTT Lab</Link>
             <div className="collapse nav-collapse">
               <ul className="navbar-nav mr-auto">
@@ -33,11 +34,45 @@ class App extends Component {
                 </li>
               </ul>
             </div>
-          </nav>
+          </nav> */}
+
+<header>
+         
+            <MDBNavbar color="bg-primary" fixed="top" dark expand="md" scrolling transparent>
+              <MDBNavbarBrand href="/">
+                <strong>Welcome!</strong>
+              </MDBNavbarBrand>
+              {!this.state.isWideEnough && <MDBNavbarToggler onClick={this.onClick} />}
+              <MDBCollapse isOpen={this.state.collapse} navbar>
+                <MDBNavbarNav left>
+                  <MDBNavItem active>
+                    <MDBNavLink to="/lab">Database</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="/dashboards">Tutorial</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="/login">Log In</MDBNavLink>
+                  </MDBNavItem>
+                </MDBNavbarNav>
+              </MDBCollapse>
+            </MDBNavbar>
+
+          <MDBView src="http://www.testnec.ro/wp-content/uploads/2019/01/imagine-2.jpg">
+            <MDBMask overlay="black-light" className="flex-center flex-column text-white text-center">
+            <h1>Welcome to SOTT Lab, home of the greatest warriors known to the Cosmos</h1>
+            </MDBMask>
+          </MDBView>
+        </header>
+
+        <main>
+          <MDBContainer className="text-center my-5">
+            <p align="justify">Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </MDBContainer>
+        </main>
       <Route path ="/dashboards" component ={Dashboard} />
       <Route path="/input" component={InputPage} />
-      {/* <Route path="/" exact component ={LoginForm} /> */}
-      <Route path="/" exact component ={SottBigBalls} />
+      <Route path="/" exact component ={LoginForm} />
       <Route path="/lab" exact component ={Table} />
       <Route path="/tutorial" component={TutorialPage} />
     </div>
