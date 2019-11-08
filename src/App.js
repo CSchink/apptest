f75310afe6fe256c424f4ff6f26eedd2feb8948c
CSchink <45950072+CSchink@users.noreply.github.com>
2019-11-08 12:23:30 -0500

    navbar original

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import 'semantic-ui-css/semantic.min.css'
 import { Button, Menu } from 'semantic-ui-react'
 
 import Dashboard from './Components/dashboards';
@@ -14,7 +15,7 @@ class App extends Component {
     <Router>
     
     <div>
-        {/* <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
             <Link to="/lab" className="navbar-brand">SOTT Lab</Link>
             <div className="collpase nav-collapse">
               <ul className="navbar-nav mr-auto">
@@ -33,25 +34,7 @@ class App extends Component {
                 </li>
               </ul>
             </div>
-          </nav> */}
-      <Menu>
-        <Menu.Item>
-        <Link to='/'><Button primary>SOTT Lab</Button></Link>
-        </Menu.Item>
-
-        <Menu.Item>
-          <Link to='/lab'><Button>Log-in</Button></Link>
-        </Menu.Item>
-
-        <Menu.Item>
-          <Link to='/input'><Button>Input</Button></Link>
-        </Menu.Item>
-
-        <Menu.Item>
-          <Link to='/tutorial'><Button>Tutorial</Button></Link>
-        </Menu.Item>
-      </Menu>
-
+          </nav>
       <Route path ="/dashboards" component ={Dashboard} />
       <Route path="/input" component={InputPage} />
       <Route path="/" exact component ={LoginForm} />
