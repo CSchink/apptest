72ceb511110657db9edc9d69eb0e24a1d8bb6b64
CSchink <45950072+CSchink@users.noreply.github.com>
2019-11-07 19:01:28 -0500

    new landing page setup

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -47,15 +47,14 @@ class SottBigBalls extends React.Component {
                 </MDBNavbarNav>
               </MDBCollapse>
             </MDBNavbar>
-          </Router>
+          
 
           <MDBView src="http://www.testnec.ro/wp-content/uploads/2019/01/imagine-2.jpg">
             <MDBMask overlay="black-light" className="flex-center flex-column text-white text-center">
             <h1>Welcome to SOTT Lab</h1>
             </MDBMask>
           </MDBView>
-        </header>
-
+        
         <main>
           <MDBContainer className="text-center my-5">
             <p align="justify">Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -66,6 +65,9 @@ class SottBigBalls extends React.Component {
       <Route path="/lab" exact component ={Table} />
       <Route path="/tutorial" component={TutorialPage} />
       <Route path="/login" component={LoginForm} />
+      </Router>
+      </header>
+
       </div>
     );
   }
